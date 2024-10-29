0aea710178ddd2989e022df81b268340ab48393a
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2024-10-29 21:06:56 +0100

    Add code for creating bucket and trigger for lambda function.
    Only after lambda function is created bucket and trigger can as well.
    
    Because cdk deploy creates all objects defined in stack in parallel. Bucket trigger tries to reference lambda function which does not exist yet and cause error. So first only lambda function needs to be created.

--- lambda-cdk-infra/lambda_cdk_infra/lambda_cdk_infra_stack.py
+++ lambda-cdk-infra/lambda_cdk_infra/lambda_cdk_infra_stack.py
@@ -31,6 +31,9 @@ class LambdaCdkInfraStack(Stack):
             repository=ecr_repository,
         )
 
+        bucket_name = "image-segmentation-bucket"
+        bucket = self.create_bucket(bucket_name)
+
         # Create new role for Lambda function
         lambda_role = iam.Role(self, "LambdaS3TriggerRole2",
                                assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
@@ -43,6 +46,32 @@ class LambdaCdkInfraStack(Stack):
                                ]
                                )
 
+        # Create an inline policy that allows S3 GetObject and PutObject actions
+        s3_access_policy = iam.Policy(self, "S3AccessPolicy",
+                                      statements=[
+                                          iam.PolicyStatement(
+                                              actions=["s3:GetObject"],
+                                              resources=[f"arn:aws:s3:::{bucket_name}/images/*"]
+                                              # Grant GetObject only on the images/ folder
+                                          ),
+                                          iam.PolicyStatement(
+                                              actions=["s3:PutObject"],
+                                              resources=[f"arn:aws:s3:::{bucket_name}/res/*"]
+                                              # Grant PutObject only on the res/ folder
+                                          )
+                                      ]
+                                      )
+
+        # Attach the inline policy to the Lambda role
+        s3_access_policy.attach_to_role(lambda_role)
+
+        # Trigger the Lambda function on .jpeg file uploads in the images/ folder
+        s3_event_source = eventsources.S3EventSource(
+            bucket,
+            events=[s3.EventType.OBJECT_CREATED],  # Trigger on object creation
+            filters=[s3.NotificationKeyFilter(prefix="images/", suffix=".jpeg")]  # Only .jpeg in images/ folder
+        )
+
         api_function = DockerImageFunction(
             self, "ImageSegmentationOnS3Upload",
             function_name="ImageSegmentationOnS3Upload",
@@ -52,10 +81,18 @@ class LambdaCdkInfraStack(Stack):
             environment={
                 "ENVIRONMENT": "production",
             },
+            events=[s3_event_source],
             role=lambda_role,
             retry_attempts=0,
             max_event_age=Duration.minutes(1),
         )
+
+        function_url = api_function.add_function_url(
+            auth_type=FunctionUrlAuthType.NONE
+        )
+
+        CfnOutput(self, "FunctionUrl", value=function_url.url)
+        CfnOutput(self, "BucketName", value=bucket.bucket_name)
 
     def create_bucket(self, bucket_name) -> s3.Bucket:
         bucket: s3.Bucket = s3.Bucket(
