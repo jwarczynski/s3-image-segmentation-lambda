fbc68d6ac3657d256c6aa791c2b8cbd528d27f9e
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2024-11-01 21:02:25 +0100

    refactor
    remove commented out code

--- image/download_weights.py
+++ image/download_weights.py
@@ -1,22 +1,9 @@
 import transformers
 from transformers import AutoImageProcessor, AutoModelForObjectDetection
 
-# import bentoml
-#
-# model_name = "Kaludi/food-category-classification-v2.0"
 model_name = "hustvl/yolos-tiny"
-# task = "classification"
-# processor = transformers.AutoProcessor.from_pretrained(model_name)
 processor = AutoImageProcessor.from_pretrained(model_name)
-# model = transformers.AutoModelForImageClassification.from_pretrained(model_name)
 model = AutoModelForObjectDetection.from_pretrained(model_name)
-#
-# with bentoml.models.create(
-#         name=model_name.split('/')[-1],  # Name of the model in the Model Store
-# ) as model_ref:
-#     # model_ref.pack("processor", processor)
-#     # model_ref.pack("model", model)
-#     # model_ref.save('/opt/model')
+
 processor.save_pretrained('/opt/model')
 model.save_pretrained('/opt/model')
-print("Downloaded weights successfully")
